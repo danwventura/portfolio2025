f3ec49cce5841d684e3fc2ab4e7f4825df66d594
danwventura <danwventura@gmail.com>
2025-04-14 21:56:55 -0400

    Adding basic component architecture

--- portfolio2025/src/App.js
+++ portfolio2025/src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
+import Hero from './components/Hero';
+import Carousel from './components/Carousel';
+import About from './components/About';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar></NavBar>
+      <Hero></Hero>
+      <Carousel></Carousel>
+      <About></About>
+      <Contact></Contact>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ portfolio2025/src/components/Carousel.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Carousel = () => {
+    return (
+      <div>
+        <h1>This is the Carousel</h1>
+      </div>
+    );
+  };
+  
+  export default Carousel;
